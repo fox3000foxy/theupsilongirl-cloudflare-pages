48d5469e89431a05f629b16a7ea75f9632ececdb
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-03-06 09:20:29 +0100

    updated eslint mistake

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
